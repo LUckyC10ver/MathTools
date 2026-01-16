85daef91113684fc8f85ad8e7e6aa16f5930b932
LUckyC10ver <shiori.yuki618@gmail.com>
2026-01-16 16:07:46 +0800

    Add multivariate grid and interpolator base classes

---
+++ Math.Core/Grids/BCMultivariateFunction.cs
@@ -0,0 +1,25 @@
+using System.IO;
+
+namespace Math.Core.Grids
+{
+    /// <summary>
+    /// 多变量函数抽象基类。
+    /// </summary>
+    public abstract class BCMultivariateFunction<TArgs, TResult>
+    {
+        /// <summary>
+        /// 返回类名称。
+        /// </summary>
+        public abstract string ClassName { get; }
+
+        /// <summary>
+        /// 输出对象结构信息。
+        /// </summary>
+        public abstract void Output(TextWriter writer);
+
+        /// <summary>
+        /// 计算函数值。
+        /// </summary>
+        public abstract TResult Evaluate(TArgs argument);
+    }
+}
---
+++ Math.Core/MathCoreInfo.cs
@@ -0,0 +1,34 @@
+namespace Math.Core
+{
+    public static class MathCoreInfo
+    {
+        public const string Name = "Math.Core";
+
+        public const double BcEpsEquidistantCenterLinearSpline = 1e-11;
+        public const double BcEpsEquidistantLinearSpline = 1e-11;
+
+        /// <summary>
+        /// Maximal number of iterations.
+        /// </summary>
+        public const int BcFunctionalAlgorithmsMaxIterations = 20;
+
+        /// <summary>
+        /// Relative precision for wanted result.
+        /// </summary>
+        public const double BcFunctionalAlgorithmsEpsilon = 1e-6;
+
+        public const double BcInfinity = 1e30;
+
+        /// <summary>
+        /// Maximale Ordnung der Polynome.
+        /// </summary>
+        public const int NpMax = 10;
+
+        public const double Pi = 3.1415926;
+
+        /// <summary>
+        /// Degrees Celsius to Kelvin offset.
+        /// </summary>
+        public const double Kelvin = 273.15;
+    }
+}
